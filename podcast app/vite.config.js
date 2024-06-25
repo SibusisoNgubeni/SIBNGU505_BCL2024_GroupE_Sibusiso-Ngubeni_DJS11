6ba963df11fae2bcd36613b27086be60a63df60d
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-06-25 12:02:56 +0200

    test

--- podcast app/vite.config.js
+++ podcast app/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()] 
+  plugins: [react()] ,
+  build: {
+    index: './src/index.html'
+  }
 })
